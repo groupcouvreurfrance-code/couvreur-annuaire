b704ee717e9ffed87d2a26c8cce4f073e5e56818
Salif Biaye <salifbiaye@esp.sn>
2025-08-14 22:49:54 +0000

    reparer l'erreur fetch au landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import {CouvreurServicesSection} from "@/components/couvreur-services-section";
 import {CouvreurAdvantagesSection} from "@/components/couvreur-avantages-section";
 import {CouvreurHeroSection} from "@/components/hero-section";
 
-export default async function HomePage() {
-    await fetch('/api/revalidate/departments', { method: 'POST' })
+export default  function HomePage() {
+
   return (
     <div className="min-h-screen">
       <Header />
